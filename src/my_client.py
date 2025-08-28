6f9e00cf3631cd9cc87cfe05da617a84f4335fbd
Peter Eijgermans <peter.eijgermans@ka.prorail.nl>
2025-08-28 09:24:17 +0200

    client

--- src/my_client.py
+++ src/my_client.py
@@ -2,11 +2,11 @@ from fastmcp import Client
 import asyncio
 
 async def demo():
-    client = Client("my_server.py")  # Could be STDIO, HTTP, etc.
+    client = Client("my_server.py")
     async with client:
         tools = await client.list_tools()
         print("Available tools:", tools)
         result = await client.call_tool("divide", {"a": 49, "b": 7})
-        print("divide(6, 7) =", result)
+        print("divide(49, 7) =", result)
 
 asyncio.run(demo())
